22bc91e2eddcf5ee433bf3da429b483d940cf44f
kalloyan <kalloyan@motorola.com>
2016-08-25 06:12:24 -0500

    Adding k_aliases and env for lp

---
+++ kload_env_lp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
+export JAVAC_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export ANDROID_JAVA_HOME=$JAVA_HOME
+export PATH=$JAVAC_HOME/bin:$JAVA_HOME/bin:$PATH
